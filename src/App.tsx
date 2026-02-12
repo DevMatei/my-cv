b27c377a0dbcdc546cb8ae444fe4ff1bd9c4a7a5
DevMatei <100729096+DevMatei@users.noreply.github.com>
2026-02-12 19:59:55 +0200

    Resolve merge conflict: prefer origin/main for src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
-import Services from './pages/Services';
 import NotFound from './pages/NotFound';
 
 function AppContent() {
@@ -21,7 +20,6 @@ function AppContent() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
